56be82ab73d61db5b29515e1c0b74034a87de585
ElenaOanca <elenaelena.oanca@gmail.com>
2023-10-11 18:56:48 +0100

    s3g3

--- m1/s3/giorno 3/JS/script.js
+++ m1/s3/giorno 3/JS/script.js
@@ -1,60 +1,47 @@
 
+const inputTask = document.getElementById("inputTask");
+const addButton = document.querySelector(".bottoneAggiungi");
+const taskList = document.querySelector(".lista");
 
-document.addEventListener("DOMContentLoaded", function() {
-    const inputTask = document.getElementById("inputTask");
-    const addButton = document.querySelector(".bottoneAggiungi");
-    const taskList = document.querySelector(".lista");
 
-    addButton.addEventListener("click", function() {
-        const taskText = inputTask.value.trim();
-        if (taskText !== "") {
-            addTask(taskText);
-            inputTask.value = "";
-        }
-    });
+function createNewTask(text) {
+  const li = document.createElement("li");
+  const taskText = document.createElement("span");
+  taskText.textContent = text;
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "Elimina";
+  li.appendChild(taskText);
+  li.appendChild(deleteButton);
+  taskList.appendChild(li);
 
-    inputTask.addEventListener("keydown", function(event) {
-        if (event.key === "Enter") {
-            const taskText = inputTask.value.trim();
-            if (taskText !== "") {
-                addTask(taskText);
-                inputTask.value = "";
-            }
-        }
-    });
+ 
+  li.addEventListener("click", function () {
+    li.classList.toggle("completato");
+  });
 
-    taskList.addEventListener("click", function(event) {
-        if (event.target.classList.contains("delete-task")) {
-            removeTask(event.target.parentElement);
-        } else if (event.target.tagName === "LI") {
-            toggleComplete(event.target);
-        }
-    });
+  deleteButton.addEventListener("click", function () {
+    taskList.removeChild(li);
+  });
 
-    /*addButtonComplete.addEventListener("click", function() {  
-    if('click'){
-        let bottonComplete = document.getElementByClass('complete-task');
-        bottonComplete.onclick = function () {
-            newTask.style.textDecoration = "line-through";
-        }});*/
 
-    function addTask(taskText) {
-        const li = document.createElement("li");
-        li.textContent = taskText;
+  inputTask.value = "";
+}
 
-        li.innerHTML += '<button class="delete-task">Elimina</button> <button class="complete-task">Complete</button>';
-        
-        taskList.appendChild(li);
-    }
-
-    function removeTask(taskElement) {
-        taskElement.remove();
-    }
-
-    function toggleComplete(taskElement) {
-        taskElement.classList.toggle("completato");
-    }
+addButton.addEventListener("click", function () {
+  const taskText = inputTask.value.trim();
+  if (taskText !== "") {
+    createNewTask(taskText);
+  }
 });
 
+
+inputTask.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    const taskText = inputTask.value.trim();
+    if (taskText !== "") {
+      createNewTask(taskText);
+    }
+  }
+});
 
 
