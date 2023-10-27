0e20d5d01562f4b90712d522ba874d5255135838
ElenaOanca <elenaelena.oanca@gmail.com>
2023-10-27 20:50:41 +0100

    m2-s5-l5

--- m2/s1/progetto-m2-s1/script.js
+++ m2/s1/progetto-m2-s1/script.js
@@ -1,19 +1,23 @@
 const header = document.querySelector('.sticky-header');
 const section1 = document.querySelector('.section1');
-
+const buttonStarted =document.querySelector('.cambia');
 // Ascolta l'evento di scorrimento della pagina
 window.addEventListener('scroll', () => {
     const headerHeight = header.getBoundingClientRect().height;
     const section1Top = section1.getBoundingClientRect().top;
 
     // Calcola la posizione in cui la sezione 1 è scrollata oltre 2/3 sotto l'intestazione
-    const scrollThreshold = 2 * headerHeight / 3;
+    const scrollThreshold = 2 * headerHeight / 2;
 
     if (section1Top < scrollThreshold) {
         // La sezione 1 è stata scrollata oltre 2/3 sotto l'intestazione, cambia il colore di sfondo
-        header.style.backgroundColor = 'trasparent'; // Sostituisce  il colore 
+        header.style.backgroundColor = 'white';// Sostituisce  il colore 
+        buttonStarted.style.backgroundColor ='green'
+
     } else {
         // Altrimenti, ripristina il colore di sfondo predefinito
         header.style.backgroundColor = '#ffc017';
+        buttonStarted.style.backgroundColor ='#191919'
+
     }
 });
