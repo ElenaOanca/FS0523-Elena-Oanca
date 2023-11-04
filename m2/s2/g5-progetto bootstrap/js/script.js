cd6f5c2ca61c2c1824666706cb817b501f2731f5
ElenaOanca <elenaelena.oanca@gmail.com>
2023-11-04 13:59:56 +0100

    m2-s6-netflix-hover e js

--- m2/s2/g5-progetto bootstrap/js/script.js
+++ m2/s2/g5-progetto bootstrap/js/script.js
@@ -1,7 +1,59 @@
 $(document).ready(function(){
-    // Event listener per il click sul bottone Toggle
+    
     $("#toggleButton").on("click", function() {
-        // Alterna la visualizzazione del menu
+      
         $("#navbarSupportedContent").slideToggle();
     });
 });
+
+/*
+document.addEventListener('DOMContentLoaded', function() {
+    // Trova tti 'a'  navbar
+    const navLinks = document.querySelectorAll('.navbar-nav a');
+
+    //ciascun click evento
+    navLinks.forEach(link => {
+        // evento al pass del mouse sopra a
+        link.addEventListener('mouseover', function() {
+            // Ingrandisci il testo
+            link.style.fontSize = '20px';
+            
+        });
+
+        // evento al passaggio del mouse fuori a
+        link.addEventListener('mouseout', function() {
+            // Ripristina impost predef
+            link.style.fontSize = '';
+            link.style.color = '';
+        });
+    });
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
