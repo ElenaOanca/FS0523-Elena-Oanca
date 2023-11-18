65ee077eac70e772b7c79991731be4a5db08947a
ElenaOanca <elenaelena.oanca@gmail.com>
2023-11-18 11:39:11 +0100

    Add additional numbers to numericArray

--- m1/s2/PROGETTO SETTIMANA 2/JS/script.js
+++ m1/s2/PROGETTO SETTIMANA 2/JS/script.js
@@ -140,7 +140,8 @@ for (let i = 0; i < cars.length; i++) {
     Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
 */
 const numericArray = [
-    6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
+    6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105, 3, 67, 21, 1, 9, 0,
+
 ]
 
 while ( i < numericArray.length && numericArray[i] !== 32 ) {
