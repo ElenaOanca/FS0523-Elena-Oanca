7d7112e97537357e4d9e3dce3e956595090c150f
ElenaOanca <elenaelena.oanca@gmail.com>
2023-11-25 10:59:09 +0100

    progetto-m3-s1-g5

--- m3/s9/g5/esercizi/src/index.ts
+++ m3/s9/g5/esercizi/src/index.ts
@@ -1,21 +1,33 @@
+import "colors";
+
+interface Call {
+    calledNumber: string;
+    date: Date;
+    duration: number;
+    cost: number;
+}
 
 interface Cellular {
     balance: number;
     costPerMinute: number;
     numcall: number;
-
+    listCall: Call[];
     recharge(euro: number): void;
     number404(): string;
     getNumberCall(): number;
-    call(min:number): void;
+    call(min:number,calledNumber:string): void;
     deleteCallList(): void;
+
+
 }
+
 
 //classe Phone che implementa l'interfaccia Cellular
 class Phone implements Cellular {
     numcall: number;
     balance: number;
     costPerMinute: number=0.20;
+    listCall: Call[]=[];
   
     constructor(balance: number, costPerMinute: number,numcall:number) {
         this.balance = balance;
@@ -25,7 +37,7 @@ class Phone implements Cellular {
     }
     recharge(euro: number): void {
         this.balance += euro;
-        console.log("Il credito residuo è: " + this.balance + "€");
+      
         
     }
    
@@ -39,25 +51,53 @@ class Phone implements Cellular {
     }
    
   
-    call(min: number): void {
+    call(min: number,calledNumber:string): void {
         this.balance -= min * this.costPerMinute;
         this.numcall++;
-        console.log("Il credito residuo è: " + this.balance + "€");
+        let callDate = new Date();
+        callDate.setMinutes(0,0)
+        callDate.setMilliseconds(0);
+
+        let call: Call = {
+            calledNumber: calledNumber,
+            date: callDate,
+            duration: min,
+            cost: min * this.costPerMinute
+        };
+        this.listCall.push(call);
     }
+
+
   
     deleteCallList(): void {
         this.numcall = 0;
+        this.listCall = [];
     }
+
+    //filtro chiamate effettuate per data
+    filterCallList(date: Date): Call[] {
+        return this.listCall.filter(call => {
+           
+            return call.date.getTime() == date.getTime();
+            
+            
+        });
+    }   
+
+    showCallList(): string {
+        let result: string = "";
+        this.listCall.forEach(call => {
+            result += "called number: ".green + call.calledNumber + " date: " + call.date + " duration: " + call.duration + " cost: " + call.cost + "\n";
+        });
+        return result;
+
+    }
+
 }
 
 
 
-interface Call {
-    calledNumber: string;
-    date: Date;
-    duration: number;
-    cost: number;
-}
+
 
 //instanzio 3 cell
 let phone1: Phone = new Phone(50, 0.20,9);
@@ -70,11 +110,26 @@ phone1.recharge(10);
 phone2.recharge(20);
 phone3.recharge(30);
 
-phone1.call(5);
-phone2.call(10);
-phone3.call(15);
+phone1.call(5, "333333335656533");
 
-//mostro ù residuo
+phone1.call(10, "55555335656533");
+phone2.call(5, "2256533");
+phone2.call(10, "1135656533");
+phone2.call(10, "32665965656333");
+phone3.call(15, "33556566565533");
+
+let callFilter = phone1.filterCallList(new Date(2023,10,25,10,0,0));
+// console.log(callFilter);
+
+phone1.showCallList();
+console.log(phone1.showCallList());
+
+console.log(phone1.listCall);
+console.log(phone2.listCall);
+console.log(phone3.listCall);
+
+
+// mostro residuo
 
 console.log(phone1.number404());
 console.log(phone2.number404());
@@ -86,78 +141,12 @@ console.log(phone1.getNumberCall());
 console.log(phone2.getNumberCall());
 console.log(phone3.getNumberCall());
 
-//cancello chiamate
+// cancello chiamate
 
 phone1.deleteCallList();
 phone2.deleteCallList();
 phone3.deleteCallList();
 
-
-//extra------------------------------------------------------------------------------------
-interface CallList {
-    id: number;
-    duration: number;
-    date: Date;
-    time: Date;
-}
-
-//array di oggetti
-
-let callList: CallList[] = [
-    { id: 1, duration: 5, date: new Date(2020, 10, 10), time: new Date(2020, 10, 10, 10, 10, 10) },
-    { id: 2, duration: 10, date: new Date(2020, 10, 10), time: new Date(2020, 10, 10, 10, 10, 10) },
-    { id: 3, duration: 15, date: new Date(2020, 10, 10), time: new Date(2020, 10, 10, 10, 10, 10) },
-    { id: 4, duration: 20, date: new Date(2020, 10, 10), time: new Date(2020, 10, 10, 10, 10, 10) },
-    { id: 5, duration: 25, date: new Date(2020, 10, 10), time: new Date(2020, 10, 10, 10, 10, 10) }
-];
-
-//mostro  lista  chiamate
-
-console.log(callList);
-
-//funz calcola costo  chiamata
-function costCall(callList: CallList[], costPerMinute: number): number {
-    let cost: number = 0;
-    for (let i = 0; i < callList.length; i++) {
-        cost += callList[i].duration * costPerMinute;
-    }
-    return cost;
-}
-
-//mostro  costo  chiamata
-
-console.log(costCall(callList, 0.20));
-
-//funzione  cancella  lista/chiamate
-
-function deleteCall(callList: CallList[]): void {
-    callList.splice(0, callList.length);
-
-}
-
-//cancello lista chiamate
-
-deleteCall(callList);
-
-//list call
-
-console.log(callList);
-
-//funz cancella  chiamate x id
-
-function deleteCallId(callList: CallList[], id: number): void {
-    for (let i = 0; i < callList.length; i++) {
-        if (callList[i].id == id) {
-            callList.splice(i, 1);
-        }
-    }
-}
-
-//cancello chiamata  id
-
-deleteCallId(callList, 3);
-
-//list call
-
-console.log(callList);
-
+console.log(phone1.listCall);
+console.log(phone2.listCall);
+console.log(phone3.listCall);
