518f72d499eb73ca9ebe43985fa21cead91645a8
ElenaOanca <elenaelena.oanca@gmail.com>
2023-12-09 21:34:13 +0100

    progetto-Meteo

--- m3/s11/ultimo-progettoFE/meteo/src/app/pages/home/home.module.ts
+++ m3/s11/ultimo-progettoFE/meteo/src/app/pages/home/home.module.ts
@@ -1,12 +1,8 @@
-import { HomeComponent } from './../../../../../Nuova cartella/src/app/pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
-//import form from '@angular/forms';
-
-
-
+import { HomeComponent } from './home.component';
 
 
 @NgModule({
